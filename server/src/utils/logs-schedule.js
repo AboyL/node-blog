15699cc6ea4826bf38a6e2bc5a746f204fe6abfa
L <1053801783@qq.com>
2020-01-11 22:10:01 +0800

    修改了错误的定时任务

--- server/src/utils/logs-schedule.js
+++ server/src/utils/logs-schedule.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule');
 const fs = require('fs-extra')
 const path = require('path')
 
-schedule.scheduleJob('0 * * * *', function () {
+schedule.scheduleJob('0 * * *', function () {
   console.log('chunk access log');
   // 进行复制操作
   const d = new Date()
